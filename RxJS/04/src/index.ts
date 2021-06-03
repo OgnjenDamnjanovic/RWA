0f771b2ac626b29d52562590459c800b43598bfd
OgnjenDamnjanovic <ognjen.damnjanovic@elfak.rs>
2021-06-03 19:28:17 +0200

    04 final

--- RxJS/04/src/index.ts
+++ RxJS/04/src/index.ts
@@ -1,17 +1,17 @@
-import { concat, from, interval, Observable, of, range, timer } from "rxjs";
+import { concat, from, Observable, of, range, timer } from "rxjs";
 import { concatMap, delay, endWith, map, repeat, startWith } from "rxjs/operators";
 
-const lightTaskBlockObs: Observable<string> = from([1,2,3,4,5,6]).pipe(
+const lightInstructionsBlockObs: Observable<string> = from([1,2,3,4,5,6]).pipe(
   concatMap(number=>of(number).pipe(
     delay(500)
   )),
   map((num) => `light task no. ${num} ended`),
   
 );
-const heavyTaskObs: Observable<string> = timer(3000).pipe(
+const heavyInstructionObs: Observable<string> = timer(3000).pipe(
   map((num) => `heavy task ended`)
 );
-const lightTaskLoopObs: Observable<string> = range(1,3).pipe(
+const lightInstructionLoopObs: Observable<string> = range(1,3).pipe(
   concatMap(number=>of(number).pipe(
     delay(200)
     )),
@@ -20,7 +20,7 @@ const lightTaskLoopObs: Observable<string> = range(1,3).pipe(
       repeat(3)
 );
 
-concat( lightTaskBlockObs, heavyTaskObs, lightTaskLoopObs).pipe(
+concat( lightInstructionsBlockObs, heavyInstructionObs, lightInstructionLoopObs).pipe(
     startWith("program start"),
     endWith("end of program")
 ).subscribe(result => console.log(result));
